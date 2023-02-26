abb14a2a28f39f9edcba6fac5b31ba2953f635cc
chintan <chintan1612199@gmail.com>
2023-02-26 17:08:26 +0530

    create router and model schema

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose=require('mongoose')
 const dotenv =require('dotenv')
 const helmet =require('helmet')
 const morgan=require('morgan')
+const userRoute=require('./routes/users')
+const userAuth=require('./routes/auth')
+
 
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL,
@@ -16,6 +19,8 @@ mongoose.connect(process.env.MONGO_URL,
 app.use(express.json())
 app.use(helmet())
 app.use(morgan('common'))
+app.use('/api/users',userRoute)
+app.use('/api/auth/',userAuth)
 
 
-app.listen(8800)    
+app.listen(5000)    
