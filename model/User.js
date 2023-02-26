734ed99a306d7fdff91638f8b6b10a6efd4ad179
chintan <chintan1612199@gmail.com>
2023-02-26 17:27:01 +0530

    model exports in User model.

--- model/User.js
+++ model/User.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema({
 
      username: {
@@ -38,5 +39,11 @@ const UserSchema = new mongoose.Schema({
      isAdmin: {
           type: Boolean,
           default: false
-     }
-})
+     },
+
+},
+     { timestamps: true },
+
+)
+
+module.exports = mongoose.model('User', UserSchema)
