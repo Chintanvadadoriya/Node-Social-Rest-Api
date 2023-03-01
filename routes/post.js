d4ba86f19104ff9707572b061eefd6d73d368e69
chintan <chintan1612199@gmail.com>
2023-03-01 22:42:40 +0530

    make friend to friend show post api

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,6 @@
 const router =require('express').Router()
 const Post =require('../model/Post')
+const User=require('../model/User')
 
 // create post
 router.post('/',async(req,res)=>{
@@ -62,5 +63,28 @@ router.put('/:id/like',async(req,res)=>{
      }
 })
 // get post
+router.get('/:id',async(req,res)=>{
+     try{
+     const post  =await Post.findById(req.params.id)
+     res.status(200).json(post)
+     }catch(err){
+          console.log('err in Get post :>> ', err);
+          res.status(500).json(err)
+     }
+})
 //get timeline post
+router.get('/timeline/all',async(req,res)=>{
+     try{
+          const currentUser =await User.findById(req.body.userId );
+          const userPost =await Post.find({userId:currentUser._id})
+          const friendPost =await Promise.all(currentUser.followings.map((friendId)=>{ return Post.find({userId:friendId})}))
+          res.json(userPost.concat(...friendPost))
+     }catch(err){
+          console.log('Timeline Exeption :>> ', err);
+          res.status(500).json(err)
+     }
+})
 module.exports=router
+
+
+
